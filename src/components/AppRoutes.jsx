f95556ae4cee856225fbeccf0966c689449ff7d6
Mabel Lawrence <mabelmail3@gmail.com>
2024-01-15 15:59:11 -0500

    project page overview

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutMe from './LandingPage';
+import Projects from './Projects';
 import NotFound from './NotFound.jsx';
 
 const AppRoutes = () => {
@@ -9,7 +10,7 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route exact path="/" element={<AboutMe />} />
-        {/* <Route exact path="/resume" element={<Resume />} /> */}
+        <Route exact path="/projects" element={<Projects />} />
         <Route path="*" element={<AboutMe />} />
       </Routes>
     </Router>
